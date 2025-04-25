3c1286613124083fb586acdfe1a622decfe9caf1
schmelmann <legendishaanxop@gmail.com>
2025-04-25 16:13:12 +0530

    Update card.js

--- src/components/ui/card.js
+++ src/components/ui/card.js
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
 export const Card = ({ children, className }) => (
-    <div className={`rounded-lg border p-4 bg-white ${className || ""}`}>
-      {children}
-    </div>
-  );
-  
-  export const CardContent = ({ children, className }) => (
-    <div className={className}>{children}</div>
-  );
-  
-=======
-export const Card = ({ children, className }) => (
-    <div className={`rounded-lg border p-4 bg-white ${className || ""}`}>
-      {children}
-    </div>
-  );
-  
-  export const CardContent = ({ children, className }) => (
-    <div className={className}>{children}</div>
-  );
-  
->>>>>>> 2dd9a343bcbcb31d851a7954bae252dabbb3d1ec
-  
+  <div className={`rounded-lg border p-4 bg-white ${className || ""}`}>
+    {children}
+  </div>
+);
+
+export const CardContent = ({ children, className }) => (
+  <div className={className}>{children}</div>
+);
