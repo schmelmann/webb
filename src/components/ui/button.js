f08a5884d3584756c9fe80db73f088047e8b4867
schmelmann <legendishaanxop@gmail.com>
2025-04-25 16:12:03 +0530

    Update button.js

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
 export const Button = ({ children, ...props }) => (
-    <button
-      {...props}
-      className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded"
-    >
-      {children}
-    </button>
-=======
-export const Button = ({ children, ...props }) => (
-    <button
-      {...props}
-      className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded"
-    >
-      {children}
-    </button>
->>>>>>> 2dd9a343bcbcb31d851a7954bae252dabbb3d1ec
-  );
+  <button
+    {...props}
+    className="bg-pink-500 hover:bg-pink-600 text-white font-bold py-2 px-4 rounded"
+  >
+    {children}
+  </button>
+);
