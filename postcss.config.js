848178ba508d7b68e8685ed88843c9f3ee2684e3
schmelmann <legendishaanxop@gmail.com>
2025-04-25 16:39:15 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
+// postcss.config.js
+
 module.exports = {
   plugins: [
-    'tailwindcss', // Add Tailwind CSS plugin
-    'autoprefixer', // Add Autoprefixer plugin for better browser support
-  ],
-};
+    require('postcss'),
+    require('tailwindcss'),
+    // other plugins...
+  ]
+}
